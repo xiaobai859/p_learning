3616f21f3870a4db8b6524291596ac8af9acd166
xiaobai <xiaobaicai@xiaobai.com>
2019-06-12 20:36:25 +0800

    把本地工程添加到git

---
+++ git_project/src/com/itheim/git/GitClass.java
@@ -0,0 +1,17 @@
+package com.itheim.git;
+
+public class GitClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+
+
+}
