5543aaddcca546fa95846c06b1d1a19b210471e5
xiaobai <xiaobaicai@xiaobai.com>
2019-06-12 21:08:29 +0800

    添加一个demo类

---
+++ git_project/src/com/itheim/git/DemoClass.java
@@ -0,0 +1,14 @@
+package com.itheim.git;
+
+public class DemoClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello 222");
+        System.out.println("hello 222");
+        System.out.println("hello 222");
+        System.out.println("hello 222");
+        System.out.println("hello 222");
+        System.out.println("hello 222");
+        System.out.println("hello 222");
+    }
+}
