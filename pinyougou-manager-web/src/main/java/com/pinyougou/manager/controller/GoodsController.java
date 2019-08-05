a8d4a856b5f854192c73b6417379a1f223fae1c0
xiaobai <xiaobaicai@xiaobai.com>
2019-08-05 19:52:53 +0800

    删除索引库2

--- pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/GoodsController.java
+++ pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/GoodsController.java
@@ -2,7 +2,6 @@ package com.pinyougou.manager.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +14,7 @@ import com.pinyougou.sellergoods.service.GoodsService;
 
 import entity.PageResult;
 import entity.Result;
+
 /**
  * controller
  * @author Administrator
